30c9dfdb478043fd793906d0aa8b1e83d60f64a6
MD ASHIKUR RAHMAN ASHIK <md.ashikur.upwork@gmail.com>
2024-02-02 21:12:22 +0600

    Learning functions

---
+++ function.js
@@ -0,0 +1,23 @@
+const number = 45;
+function fanOff(){
+    console.log("Stand up from your seat.");
+    console.log("Walk towards the switch");
+    console.log("Turn off the switch");
+}
+
+// fanOff();
+
+function brushTeeth(){
+    console.log("Pick up the brush");
+    console.log("Add paste");
+    console.log("Brush for 2 minutes");
+}
+
+// brushTeeth();
+
+function eatRice(){
+    console.log("Wash your hand");
+    console.log("Sit down");
+    console.log("Take meal to your plate");
+    console.log("Begin eating your meal.");
+}
