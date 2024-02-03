efc67c79ba2817b4c46bd4e60ea67cf860b9e9a5
MD ASHIKUR RAHMAN ASHIK <md.ashikur.upwork@gmail.com>
2024-02-03 11:50:04 +0600

    Recapping the module

--- function.js
+++ function.js
@@ -25,7 +25,48 @@ function eatRice(){
 // eatRice();
 
 function square(number){
-    console.log(number);
+    // console.log(number);
+    // console.log("Value of the number parameter is:", number);
+    const square = number * number;
+    // console.log("The square of the number parameter is:", square);
 }
-console.log("Value of the number parameter is:", number);
 square(4);
+
+function add(num1, num2){
+    const sum = num1 + num2;
+    // console.log("The first number is:", num1);
+    // console.log("The second number is:", num2);
+    // console.log("The sum of two numbers is:", sum);
+}
+
+add(5, 7);
+
+function addAll(num1, num2, num3, num4, num5){
+    const total = num1 + num2 + num3 + num4 + num5;
+    // console.log("The sum of five numbers is:", total);
+}
+
+addAll(1, 2, 3, 4, 5);
+
+function doubleIt(number){
+    console.log("The current number is", number);
+    const doubled = number *2;
+    console.log("The double of current number is:", doubled);
+}
+
+// doubleIt(5);
+
+const number = 55;
+// doubleIt(number);
+
+const money = 112;
+// doubleIt(money);
+
+function difference(num1, num2){
+    console.log("The first number is:", num1);
+    console.log("The second number is:", num2);
+    const difference = num1 - num2;
+    console.log("The difference between number one and number two is:", difference);
+}
+
+difference(10, 5);
---
+++ return.js
@@ -0,0 +1,45 @@
+function tenTimes(number){
+    const result = number * 10;
+    return result;
+}
+
+// tenTimes(10);
+
+function cutHalf(number){
+    const result = number / 2;
+    return result;
+}
+
+// const result = tenTimes(5);
+// console.log(result);
+
+function add(num1, num2){
+    console.log("The first number is:", num1);
+    console.log("The second number is:", num2);
+    const total = num1 + num2;
+    return total;
+}
+
+// const result = add(5, 10);
+// console.log("The sum of two number is:", result);
+
+function addAll(price1, price2){
+    console.log("The price of the first item is:", price1);
+    console.log("The price of the second item is:", price2);
+    return price1 + price2;
+}
+
+// const result = addAll(10, 15);
+// console.log("The bill is:", result);
+
+function doMath(num1, num2){
+    const sum = num1 + num2;
+    const multiply = num1 * num2;
+    const divide = num1 / num2;
+    const difference = num1 - num2;
+    return difference;
+}
+
+function isEven(number){
+    
+}
