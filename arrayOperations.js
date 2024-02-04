f2bb666232cad53af20935e5d52f63bd8ae8fa8d
MD ASHIKUR RAHMAN ASHIK <md.ashikur.upwork@gmail.com>
2024-02-04 15:23:34 +0600

    Module finished

--- arrayOperations.js
+++ arrayOperations.js
@@ -1,7 +1,7 @@
 function sumOfNumbers(numbers){
     let sum = 0;
     for(let number of numbers){
-        console.log(number);
+        // console.log(number);
         sum = number + sum;
         // number++;
     }
@@ -9,5 +9,42 @@ function sumOfNumbers(numbers){
 }
 
 const nums = [54, 62, 12, 6];
-const sum = sumOfNumbers(nums);
-console.log("The sum of the numbers is:", sum);
+// const sum = sumOfNumbers(nums);
+// console.log("The sum of the numbers is:", sum);
+
+function evenOnly(numbers){
+    const evens = [];
+    for(const number of numbers){
+        // console.log(number);
+        if(number % 2 === 0){
+            console.log(number);
+            evens.push(number);
+        }
+    }
+    return evens;
+}
+
+// const numbers = [5, 8, 91, 24, 6];
+// const evens = evenOnly(numbers);
+// console.log(evens);
+
+function sumOfEvenNumbers(numbers){
+    let sum = 0;
+    for(const number of numbers){
+        // console.log(number);
+        if(number % 2 === 0){
+            // console.log(number);
+            sum = sum + number;
+        }
+    }
+    return sum;
+}
+
+const numbers = [1, 2, 3, 4, 5];
+const sum = sumOfEvenNumbers(numbers);
+// console.log("The sum of even numbers is:", sum);
+
+function getMenu() {
+    console.log("Burger, Pizza, Pasta");
+   }
+getMenu();   
